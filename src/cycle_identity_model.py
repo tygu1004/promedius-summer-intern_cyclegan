d72964ae1e7c6b0df427f34d801d4b172aa22c28
장동주 <tygu1004@naver.com>
2020-07-10 15:50:21 +0900

    revise test function
    학습된 모델을 이용해서 테스트하고 결과를 넘파이로 저장하는 부분을 2.x
    버전에 맞게 수정하였습니다. 저장되는 결과의 shape는
    [1, whole_size, whole_size, 1] 입니다.

--- src/cycle_identity_model.py
+++ src/cycle_identity_model.py
@@ -13,21 +13,15 @@ All rights reserved.
 from __future__ import division
 import os
 import time
-from glob import glob
 import tensorflow as tf
 import numpy as np
 from collections import namedtuple
 import cycle_identity_module as md
 import inout_util as ut
-from PIL import Image
+
 
 class cycle_identity(object):
     def __init__(self, args):
-
-        ####patients folder name
-        #         self.test_patient_no = args.test_patient_no
-        #         self.train_patient_no = [d.split('/')[-1] for d in glob(args.dcm_path + '/*') if ('zip' not in d) & (d.split('/')[-1] not in args.test_patient_no)]
-
         # save directory
         if args.taskID:
             self.taskID = args.taskID
@@ -64,14 +58,13 @@ class cycle_identity(object):
                                                       is_unpair=args.unpair, model=args.model)
             self.train_image_loader(args.train_patient_no_A, args.train_patient_no_B)
             self.test_image_loader(args.test_patient_no_A, args.test_patient_no_B)
-
+            self.patch_X_set, self.patch_Y_set = self.train_image_loader.get_train_set(args.patch_size)
+            self.whole_X_set, self.whole_Y_set = self.test_image_loader.get_test_set()
             print('data load complete !!!, {}\nN_train : {}, N_test : {}'.format(time.time() - t1,
                                                                                  len(
                                                                                      self.train_image_loader.LDCT_image_name),
                                                                                  len(
                                                                                      self.test_image_loader.LDCT_image_name)))
-            self.patch_X_set, self.patch_Y_set = self.train_image_loader.get_train_set(args.patch_size)
-            self.whole_X_set, self.whole_Y_set = self.test_image_loader.get_test_set()
 
         else:
             self.test_image_loader = ut.DCMDataLoader(args.dcm_path,
@@ -80,11 +73,10 @@ class cycle_identity(object):
                                                       image_max=args.img_vmax, image_min=args.img_vmin,
                                                       batch_size=args.batch_size,
                                                       is_unpair=args.unpair, model=args.model)
-
             self.test_image_loader(args.test_patient_no_A, args.test_patient_no_B)
+            self.whole_X_set, self.whole_Y_set = self.test_image_loader.get_test_set()
             print('data load complete !!!, {}, N_test : {}'.format(time.time() - t1,
                                                                    len(self.test_image_loader.LDCT_image_name)))
-            self.whole_X_set, self.whole_Y_set = self.test_image_loader.get_test_set()
 
         """
         build model
@@ -92,8 +84,7 @@ class cycle_identity(object):
         if args.phase == 'train':
             input_shape = (args.patch_size, args.patch_size, args.img_channel)
         else:
-            input_shape = (args.whiole_size, args.whiole_size, args.img_channel)
-        #### Generator & Discriminator
+            input_shape = (args.whole_size, args.whole_size, args.img_channel)
         # Generator
         self.generator_G = md.generator(input_shape, self.options, name="generatorX2Y")
         self.generator_F = md.generator(input_shape, self.options, name="generatorY2X")
@@ -104,7 +95,7 @@ class cycle_identity(object):
         """
         set check point
         """
-        self.ckpt = tf.train.Checkpoint(step=tf.Variable(1),
+        self.ckpt = tf.train.Checkpoint(step=tf.Variable(0),
                                         generator_G=self.generator_G,
                                         generator_F=self.generator_F,
                                         discriminator_X=self.discriminator_X,
@@ -116,49 +107,25 @@ class cycle_identity(object):
                                                                                          beta_1=args.beta1,
                                                                                          beta_2=args.beta2))
         """
-        Summary
+        Summary writer (TensorBoard)
         """
         self.writer = tf.summary.create_file_writer(self.log_dir)
 
-        '''
-        
-        #### image summary
-        self.test_G_X = self.generator(self.test_X, self.options, True, name="generatorX2Y")
-        self.test_F_Y = self.generator(self.test_Y, self.options, reuse=True, name="generatorY2X")
-
-        self.train_img_summary = tf.concat([self.patch_X, self.patch_Y, self.G_X, self.F_Y], axis=2)
-        self.summary_image_train = tf.summary.image('train_patch_image', self.train_img_summary)
-        self.test_img_summary = tf.concat([self.test_X, self.test_Y, self.test_G_X, self.test_F_Y], axis=2)
-        self.summary_image_test = tf.summary.image('test_whole_image', self.test_img_summary)
-
-        #### psnr summary
-        self.summary_psnr_input = tf.summary.scalar("1_psnr", ut.tf_psnr(self.test_X, self.test_Y, 2),
-                                                    family='PSNR')  # -1 ~ 1
-        self.summary_psnr_result_G = tf.summary.scalar("2_psnr_AtoB", ut.tf_psnr(self.test_Y, self.test_G_X, 2),
-                                                       family='PSNR')  # -1 ~ 1
-        self.summary_psnr_result_F = tf.summary.scalar("2_psnr_BtoA", ut.tf_psnr(self.test_X, self.test_F_Y, 2),
-                                                       family='PSNR')  # -1 ~ 1
-        self.summary_psnr = tf.summary.merge(
-            [self.summary_psnr_input, self.summary_psnr_result_G, self.summary_psnr_result_F])
-
-        print('--------------------------------------------\n# of parameters : {} '. \
-              format(np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()])))
-        '''
-
     def train(self, args):
-        # @tf.function
+        @tf.function
         def train_step(patch_X, patch_Y, g_optim, d_optim, step):
             with tf.GradientTape(persistent=True) as tape, self.writer.as_default():
-                # forward Generator
+                #### Forwarding
+                # Generator forward
                 G_X = self.generator_G(patch_X)
                 F_GX = self.generator_F(G_X)
                 F_Y = self.generator_F(patch_Y)
                 G_FY = self.generator_G(F_Y)
 
-                G_Y = self.generator_G(patch_Y)
-                F_X = self.generator_F(patch_X)
+                G_Y = self.generator_G(patch_Y)     # IDENT
+                F_X = self.generator_F(patch_X)     # IDENT
 
-                # forward Discriminator
+                # Discriminator forward
                 D_GX = self.discriminator_Y(G_X)
                 D_FY = self.discriminator_X(F_Y)
                 D_Y = self.discriminator_Y(patch_Y)
@@ -171,7 +138,7 @@ class cycle_identity(object):
                 G_loss_X2Y = md.least_square(D_GX, tf.ones_like(D_GX))
                 G_loss_Y2X = md.least_square(D_FY, tf.ones_like(D_FY))
 
-                G_loss = G_loss_X2Y + G_loss_Y2X + cycle_loss + identity_loss
+                G_loss = G_loss_X2Y + G_loss_Y2X + cycle_loss + identity_loss   # GAN LOSS
 
                 # discriminator loss
                 D_loss_patch_Y = md.least_square(D_Y, tf.ones_like(D_Y))
@@ -185,7 +152,6 @@ class cycle_identity(object):
 
                 #### loss summary
                 # generator
-
                 with tf.name_scope("Generator_loss"):
                     tf.summary.scalar(name="1_G_loss", data=G_loss, step=step)
                     tf.summary.scalar(name="2_cycle_loss", data=cycle_loss, step=step)
@@ -224,17 +190,27 @@ class cycle_identity(object):
             d_optim.apply_gradients(zip(discriminator_y_gradients,
                                         self.discriminator_Y.trainable_variables))
 
-        # summary test sample image during training
+        # #########################################
+        # summary train-sample image during training
         def check_train_sample(patch_X_batch, patch_Y_batch, step):
-            patch_X = tf.expand_dims(patch_X_batch[0], axis=0)
-            patch_Y = tf.expand_dims(patch_Y_batch[0], axis=0)
-            G_X = self.generator_G(patch_X)
-            F_Y = self.generator_F(patch_Y)
+            patch_X = tf.expand_dims(patch_X_batch[0], axis=0)  # Select the first patched image of batch
+            patch_Y = tf.expand_dims(patch_Y_batch[0], axis=0)  # And expand dimension to (1, patch_size, patch_size, 1)
+            G_X = self.generator_G(patch_X, training=False)     # Inference mode,
+            F_Y = self.generator_F(patch_Y, training=False)     # set training=False for normalization layer
 
-            patch_X = self.train_image_loader.rescale_arr(data=patch_X, i_min=tf.math.reduce_min(patch_X), i_max=tf.math.reduce_max(patch_X), o_min=0, o_max=255, out_dtype=tf.uint8)
-            patch_Y = self.train_image_loader.rescale_arr(data=patch_Y, i_min=tf.math.reduce_min(patch_Y), i_max=tf.math.reduce_max(patch_Y), o_min=0, o_max=255, out_dtype=tf.uint8)
-            G_X = self.test_image_loader.rescale_arr(data=G_X, i_min=tf.math.reduce_min(G_X), i_max=tf.math.reduce_max(G_X), o_min=0, o_max=255, out_dtype=tf.uint8)
-            F_Y = self.test_image_loader.rescale_arr(data=F_Y, i_min=tf.math.reduce_min(F_Y), i_max=tf.math.reduce_max(F_Y), o_min=0, o_max=255, out_dtype=tf.uint8)
+            # re-scale for Tensorboard
+            patch_X = self.train_image_loader.rescale_arr(data=patch_X, i_min=tf.math.reduce_min(patch_X),
+                                                          i_max=tf.math.reduce_max(patch_X), o_min=0, o_max=255,
+                                                          out_dtype=tf.uint8)
+            patch_Y = self.train_image_loader.rescale_arr(data=patch_Y, i_min=tf.math.reduce_min(patch_Y),
+                                                          i_max=tf.math.reduce_max(patch_Y), o_min=0, o_max=255,
+                                                          out_dtype=tf.uint8)
+            G_X = self.test_image_loader.rescale_arr(data=G_X, i_min=tf.math.reduce_min(G_X),
+                                                     i_max=tf.math.reduce_max(G_X), o_min=0, o_max=255,
+                                                     out_dtype=tf.uint8)
+            F_Y = self.test_image_loader.rescale_arr(data=F_Y, i_min=tf.math.reduce_min(F_Y),
+                                                     i_max=tf.math.reduce_max(F_Y), o_min=0, o_max=255,
+                                                     out_dtype=tf.uint8)
 
             with self.writer.as_default():
                 with tf.name_scope("check_train_sample"):
@@ -243,19 +219,31 @@ class cycle_identity(object):
                     tf.summary.image(name="G(patch_X)", step=step, data=G_X, max_outputs=1)
                     tf.summary.image(name="F(patch_Y)", step=step, data=F_Y, max_outputs=1)
 
+        # #########################################
+        # summary test-sample image during training
         def check_test_sample(step):
+            # take arbitrary sample in test_set
             buffer_size = 1000
-
             sample_whole_X = tf.constant(list(self.whole_X_set.shuffle(buffer_size).take(1).as_numpy_iterator()))
             sample_whole_Y = tf.constant(list(self.whole_Y_set.shuffle(buffer_size).take(1).as_numpy_iterator()))
+            G_X = self.generator_G(sample_whole_X, training=False)
+            F_Y = self.generator_F(sample_whole_Y, training=False)
 
-            G_X = self.generator_G(sample_whole_X)
-            F_Y = self.generator_F(sample_whole_Y)
-
-            sample_whole_X = self.train_image_loader.rescale_arr(data=sample_whole_X, i_min=tf.math.reduce_min(sample_whole_X), i_max=tf.math.reduce_max(sample_whole_X), o_min=0, o_max=255, out_dtype=tf.uint8)
-            sample_whole_Y = self.train_image_loader.rescale_arr(data=sample_whole_Y, i_min=tf.math.reduce_min(sample_whole_Y), i_max=tf.math.reduce_max(sample_whole_Y), o_min=0, o_max=255, out_dtype=tf.uint8)
-            G_X = self.test_image_loader.rescale_arr(data=G_X, i_min=tf.math.reduce_min(G_X), i_max=tf.math.reduce_max(G_X), o_min=0, o_max=255, out_dtype=tf.uint8)
-            F_Y = self.test_image_loader.rescale_arr(data=F_Y, i_min=tf.math.reduce_min(F_Y), i_max=tf.math.reduce_max(F_Y), o_min=0, o_max=255, out_dtype=tf.uint8)
+            # re-scale for Tensorboard
+            sample_whole_X = self.train_image_loader.rescale_arr(data=sample_whole_X,
+                                                                 i_min=tf.math.reduce_min(sample_whole_X),
+                                                                 i_max=tf.math.reduce_max(sample_whole_X), o_min=0,
+                                                                 o_max=255, out_dtype=tf.uint8)
+            sample_whole_Y = self.train_image_loader.rescale_arr(data=sample_whole_Y,
+                                                                 i_min=tf.math.reduce_min(sample_whole_Y),
+                                                                 i_max=tf.math.reduce_max(sample_whole_Y), o_min=0,
+                                                                 o_max=255, out_dtype=tf.uint8)
+            G_X = self.test_image_loader.rescale_arr(data=G_X, i_min=tf.math.reduce_min(G_X),
+                                                     i_max=tf.math.reduce_max(G_X), o_min=0, o_max=255,
+                                                     out_dtype=tf.uint8)
+            F_Y = self.test_image_loader.rescale_arr(data=F_Y, i_min=tf.math.reduce_min(F_Y),
+                                                     i_max=tf.math.reduce_max(F_Y), o_min=0, o_max=255,
+                                                     out_dtype=tf.uint8)
 
             with self.writer.as_default():
                 with tf.name_scope("check_test_sample"):
@@ -263,50 +251,49 @@ class cycle_identity(object):
                     tf.summary.image(name="sample_whole_Y", step=step, data=sample_whole_Y, max_outputs=1)
                     tf.summary.image(name="G(sample_whole_X)", step=step, data=G_X, max_outputs=1)
                     tf.summary.image(name="F(sample_whole_Y)", step=step, data=F_Y, max_outputs=1)
+                with tf.name_scope("PSNR"):
+                    tf.summary.scalar(name="1_psnr", step=step, data=ut.tf_psnr(sample_whole_X, sample_whole_Y, 2))  # -1 ~ 1
+                    tf.summary.scalar(name="2_psnr_AtoB", step=step, data=ut.tf_psnr(sample_whole_Y, G_X, 2))
+                    tf.summary.scalar(name="2_psnr_BtoA", step=step, data=ut.tf_psnr(sample_whole_X, F_Y, 2))
 
         # #############################
         # pre-trained model load
-        # start_step : load SUCCESS -> self.start_step 파일명에 의해 초기화... // failed -> 0
-
+        # start_step : load SUCCESS -> current_step을 checkpoint.step에 의해 초기화... // failed -> 0
         if args.continue_train:
             is_load, current_step = self.load()
             print(" [*] Load SUCCESS") if is_load else print(" [!] Load failed...")
         else:
             current_step = 0
             print(" [*] Start new train")
+        print('Start point : step : {}'.format(current_step))
 
+        # 한 에폭을 진행하는데 필요한 스탭 계산
         steps_per_epoch = min(len(self.train_image_loader.LDCT_image_name),
                               len(self.train_image_loader.NDCT_image_name)) // args.batch_size
 
-        print('Start point : step : {}'.format(current_step))
-
         # decay learning rate
-        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=args.lr,
-                                                                     decay_steps=args.decay_step,
-                                                                     decay_rate=args.decay_rate, staircase=True)
-        d_optim = tf.keras.optimizers.Adam(learning_rate=lr_schedule, beta_1=args.beta1)
-        g_optim = tf.keras.optimizers.Adam(learning_rate=lr_schedule, beta_1=args.beta1)
+        d_optim = tf.keras.optimizers.Adam(learning_rate=args.lr, beta_1=args.beta1, beta_2=args.beta2)
+        g_optim = tf.keras.optimizers.Adam(learning_rate=args.lr, beta_1=args.beta1, beta_2=args.beta2)
 
         start_time = time.time()
-        for epoch in range(args.end_epoch):
-
-            # for counting steps per epoch
-            step_count = 0
+        for epoch in range(args.epoch):
+            step_count = 0  # for counting steps per epoch
             for patch_X, patch_Y in tf.data.Dataset.zip((self.patch_X_set, self.patch_Y_set)):
-                train_step(patch_X, patch_Y, g_optim, d_optim, current_step)
+                train_step(patch_X, patch_Y, g_optim, d_optim, current_step)    # one step
+                # update step counters
                 current_step += 1
                 step_count += 1
                 self.ckpt.step.assign_add(1)
+
                 if current_step % args.print_freq == 0:
                     print(("Epoch: {} {}/{} time: {} ".format(epoch, step_count, steps_per_epoch,
                                                               time.time() - start_time)))
-                    # summary training sample image
+                    # summary with sample images
                     check_train_sample(patch_X, patch_Y, current_step)
-
-                    # check sample image
-                    # check_test_sample(current_step)
+                    check_test_sample(current_step)
 
                 if current_step % args.save_freq == 0:
+                    # checkpoint
                     self.save(args, current_step)
 
     # save model
@@ -347,22 +334,16 @@ class cycle_identity(object):
             os.makedirs(npy_save_dir)
 
         ## test
-        for idx in range(len(self.test_image_loader.LDCT_images)):
-            test_X = self.test_image_loader.LDCT_images[idx]
-
-            mk_G_X = self.sess.run(self.test_G_X,
-                                   feed_dict={self.test_X: test_X.reshape([1] + self.test_X.get_shape().as_list()[1:])})
-
+        for idx, test_X in enumerate(self.whole_X_set):
+            test_X = tf.expand_dims(test_X, axis=0)
+            mk_G_X = self.generator_G(test_X)
             save_file_nm_g = 'Gen_from_' + self.test_image_loader.LDCT_image_name[idx]
 
-            np.save(os.path.join(npy_save_dir, save_file_nm_g), mk_G_X)
+            np.save(os.path.join(npy_save_dir, save_file_nm_g), mk_G_X)  # save as shape [1, whole_size, whole_size, 1]
 
-        for idx in range(len(self.test_image_loader.NDCT_images)):
-            test_Y = self.test_image_loader.NDCT_images[idx]
-
-            mk_F_Y = self.sess.run(self.test_F_Y,
-                                   feed_dict={self.test_Y: test_Y.reshape([1] + self.test_Y.get_shape().as_list()[1:])})
-
+        for idx, test_Y in enumerate(self.whole_Y_set):
+            test_Y = tf.expand_dims(test_Y, axis=0)
+            mk_F_Y = self.generator_F(test_Y)
             save_file_nm_f = 'Gen_from_' + self.test_image_loader.NDCT_image_name[idx]
 
-            np.save(os.path.join(npy_save_dir, save_file_nm_f), mk_F_Y)
+            np.save(os.path.join(npy_save_dir, save_file_nm_f), mk_F_Y)  # save as shape [1, whole_size, whole_size, 1]
